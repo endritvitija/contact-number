7c4100ac77deab87294c46b523e95d453081e784
Endrit Vitija <vitija64@gmail.com>
2021-03-09 21:59:19 +0100

    add example file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AuthDirective } from './shared/directives/auth.directive';
+import { CapitalizePipe } from './shared/pipes/capitalize.pipe';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthDirective,
+    CapitalizePipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/components/buttons/buttons.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ButtonsComponent } from './buttons.component';
+
+
+
+@NgModule({
+  declarations: [ButtonsComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ButtonsModule { }
