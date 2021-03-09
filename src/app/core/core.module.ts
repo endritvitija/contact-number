0823cb58702ed38b5ed25eacb249ebb7c7caa828
Endrit Vitija <vitija64@gmail.com>
2021-03-09 22:22:18 +0100

    Add Material UI

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 
@@ -8,7 +12,10 @@ import { HeaderComponent } from './header/header.component';
 @NgModule({
   declarations: [FooterComponent, HeaderComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ],
   exports: [
     HeaderComponent,
